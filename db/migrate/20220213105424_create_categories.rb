50eef0187868805bd7d93ba274bc22b15ecef384
lunu <fairus200599@gmail.com>
2022-02-15 22:18:38 +0300

    part 3 fix v2

--- db/migrate/20220213105424_create_categories.rb
+++ db/migrate/20220213105424_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.string :title, default: 'Common'
+      t.string :title, null: false
 
       t.timestamps
     end
