46a51142357108f9ca68a13c23ffd2767748264f
lunu <fairus200599@gmail.com>
2022-03-02 10:25:47 +0300

    Part 5 fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  has_many :tests, inverse_of: 'author'
+  has_many :created_tests, class_name: 'Test'
 
   def started_tests_by_level(test_level)
     tests.where(level: test_level)
