cdaaa21cc221c55683f15bbd7afd8bc850dec7a2
lunu <fairus200599@gmail.com>
2022-03-05 22:55:43 +0300

    Part 6

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,3 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
-
-  default_scope { order(title: :asc) }
-
-  validates :title, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,4 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :test
-
-  validates :body, presence: true
-  validates :answers, length: { minimum: 1, maximum: 4 }
 end
