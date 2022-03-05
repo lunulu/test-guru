348388af860e5a6af6474b8817a94373391654c6
lunu <fairus200599@gmail.com>
2022-03-05 23:42:36 +0300

    Part 6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct_answers, -> { where(correct: true) }
+
+  validates :body, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :test
+
+  validates :body, presence: true
+  validates :answers, length: { minimum: 1, maximum: 4 }
 end
